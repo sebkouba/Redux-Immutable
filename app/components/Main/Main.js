cf043ccc39866cc2174096b2c9e69b8725bba0dc
Tyler McGinnis <tylermcginnis33@gmail.com>
2016-03-20 21:50:38 -0600

    Add babel, webpack, react, react route

---
+++ app/components/Main/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = React.createClass({
+  render () {
+    return (
+      <p>Hello World!</p>
+    )
+  }
+})
+
+export default Main
--- stepsWhileBuilding.js
+++ stepsWhileBuilding.js
@@ -1,5 +1,21 @@
-1) Make firebase-schema.js
-2) Make redux-schema.js
-3) Make actions.js file add all actions to it for now.
-4) Make reducers.js file and add all reducers to it for now.
-5)
+firebase-schema - Make firebase-schema.js
+redux-schema - Make redux-schema.js
+actions - Make actions.js file add all actions to it for now.
+reducers - Make reducers.js file and add all reducers to it for now.
+hello-world - Build Hello World
+    - npm init -y
+    - npm install --save react react-dom && npm install --save-dev html-webpack-plugin webpack webpack-dev-server babel-{core,loader} babel-preset-{react,es2015,stage-0}
+    - Basic webpack.config.js
+    - .babelrc
+    - mkdir app
+    - touch index.js && touch index.html
+    - Fill out index.html
+    - Add Hello World to index.js and render it.
+    - add start and prod to package.json
+react-router - Add React Router
+  - npm install --save react-router
+  - Make components folder
+  - Make Main folder
+      - Main.js and move HelloWorld code to it
+  - Make index.js of components folder
+  - Make config/routes.js and fill out routes
