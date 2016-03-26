16cbd6e56764ab4da895f9e4b7ca88085e5253a9
Tyler McGinnis <tylermcginnis33@gmail.com>
2016-03-25 23:38:34 -0600

    Webpack dynamic based on launch command

--- stepsWhileBuilding.js
+++ stepsWhileBuilding.js
@@ -22,10 +22,6 @@ react-router - Add React Router
 developer-experience
   - npm -v
   - node -v
-  - Purposefully mistype a variable name and then check the error in the cosole
-    - Gross index_bundle.js error.
-    - Add devtool: 'eval-source-map' to webpack config and restart webpack
-    - show error message again with sourcemap enabled.
   - Eslint
     - http://standardjs.com/
     - npm install --save-dev eslint eslint-{config-standard,plugin-standard,plugin-promise}
@@ -39,3 +35,31 @@ developer-experience
     - Fill out rest of eslintrc besides rules
     - Add rules
     - Fix errors but should still be one error in index.js with exporting file
+      - Mention how the export is experimental so we need to have eslint use the babel parser
+      - Add npm install babel-eslint@next --save-dev
+      - Add parser: "babel-eslint"
+  - require('path') and PATHS object in webpack.config
+  - Purposefully mistype a variable name and then check the error in the cosole
+    - Gross index_bundle.js error.
+    - Add devtool: 'eval' to webpack config and restart webpack
+    - show error message again with sourcemap enabled.
+    - But we don't want eval to run in production. In fact, we need to change quite a bit for prod.
+  - Talk about production vs development configs. Multiple ways to do it.
+  - create base, developmentConfig, and productionConfig as empty objects
+  - Production Prep (test just in module.exports)
+    - process.env.npm_lifecycle_event
+    - isProduction variable
+    - Steps with Production React
+      - initial file size ~ 873kb
+      - process.env.node_env to 'production' (do then show file size) ~833kb
+      - devtool from 'eval' -> cheap-module-source-map (do then show file size) 179kb
+      - new webpack.optimize.UglifyJsPlugin({minimize: true}) (may not be needed)
+  - Add shared config to base
+  - Add devtool: 'eval' to developmentConfig
+  - Add devtool: 'cheap-module-source-map' to productionConfig
+  - Add plugins to developmentConfig
+  - Add plugins to productionConfig
+  - Now we need a way to merge them together
+    - Change name to webpack.config.babel.js
+    - Change requires to imports and vars to consts
+    - Use Object.assign
---
+++ webpack.config.babel.js
@@ -0,0 +1,52 @@
+import webpack from 'webpack'
+import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+
+const LAUNCH_COMMAND = process.env.npm_lifecycle_event
+
+const isProduction = LAUNCH_COMMAND === 'production'
+// process.env.BABEL_ENV = LAUNCH_COMMAND
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'dist'),
+}
+
+const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: PATHS.app + '/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+const productionPlugin = new webpack.DefinePlugin({
+  'process.env': {
+    NODE_ENV: JSON.stringify('production')
+  }
+})
+
+const base = {
+  entry: [
+    PATHS.app
+  ],
+  output: {
+    path: PATHS.build,
+    filename: 'index_bundle.js'
+  },
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+    ]
+  }
+}
+
+const developmentConfig = {
+  devtool: 'eval',
+  plugins: [HTMLWebpackPluginConfig]
+}
+
+const productionConfig = {
+  devtool: 'cheap-module-source-map',
+  plugins: [HTMLWebpackPluginConfig, productionPlugin, new webpack.optimize.UglifyJsPlugin({minimize: true})]
+}
+
+export default Object.assign({}, base, isProduction === true ? productionConfig : developmentConfig)
