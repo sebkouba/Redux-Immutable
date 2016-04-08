e39843b60e8035ad4210d31b113fc447a3fe947c
Tyler McGinnis <tylermcginnis33@gmail.com>
2016-04-07 23:18:13 -0600

    Step5 in Immutable - modal

--- app/containers/Modal/ModalContainer.js
+++ app/containers/Modal/ModalContainer.js
@@ -5,12 +5,12 @@ import * as modalActionCreators from 'redux/modules/modal'
 import * as ducksActionCreators from 'redux/modules/ducks'
 
 function mapStateToProps ({modal, users, authedUser}, props) {
-  const duckTextLength = modal.duckText.length
+  const duckText = modal.get('duckText')
   return {
     user: users[users.authedId] ? users[users.authedId].info : {},
-    duckText: modal.duckText,
-    isOpen: modal.isOpen,
-    isSubmitDisabled: duckTextLength <= 0 || duckTextLength > 140,
+    duckText,
+    isOpen: modal.get('isOpen'),
+    isSubmitDisabled: duckText.length <= 0 || duckText.length > 140,
   }
 }
 
--- app/redux/modules/modal.js
+++ app/redux/modules/modal.js
@@ -1,3 +1,5 @@
+import { Map } from 'immutable'
+
 const OPEN_MODAL = 'OPEN_MODAL'
 const CLOSE_MODAL = 'CLOSE_MODAL'
 const UPDATE_DUCK_TEXT = 'UPDATE_DUCK_TEXT'
@@ -21,28 +23,19 @@ export function updateDuckText (newDuckText) {
   }
 }
 
-const initialState = {
+const initialState = Map({
   duckText: '',
   isOpen: false,
-}
+})
 
 export default function modal (state = initialState, action) {
   switch (action.type) {
     case OPEN_MODAL :
-      return {
-        ...state,
-        isOpen: true,
-      }
+      return state.set('isOpen', true)
     case CLOSE_MODAL :
-      return {
-        duckText: '',
-        isOpen: false,
-      }
+      return initialState
     case UPDATE_DUCK_TEXT :
-      return {
-        ...state,
-        duckText: action.newDuckText,
-      }
+      return state.set('duckText', action.newDuckText)
     default :
       return state
   }
