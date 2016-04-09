2943dcaa39fb466c8e76172298a12f2da60ef9b8
Tyler McGinnis <tylermcginnis33@gmail.com>
2016-04-09 05:33:27 -0600

    21-set-users-likes

--- app/containers/Main/MainContainer.js
+++ app/containers/Main/MainContainer.js
@@ -1,11 +1,24 @@
 import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import { Navigation } from 'components'
 import { container, innerContainer } from './styles.css'
+import * as usersLikesActionCreators from 'redux/modules/usersLikes'
 
 const MainContainer = React.createClass({
   propTypes: {
     isAuthed: PropTypes.bool.isRequired,
+    setUsersLikes: PropTypes.func.isRequired,
+  },
+  componentDidMount () {
+    if (this.props.isAuthed) {
+      this.props.setUsersLikes()
+    }
+  },
+  componentWillReceiveProps (nextProps) {
+    if (this.props.isAuthed !== nextProps.isAuthed) {
+      this.props.setUsersLikes()
+    }
   },
   render () {
     return (
@@ -20,5 +33,6 @@ const MainContainer = React.createClass({
 })
 
 export default connect(
-  ({users}) => ({isAuthed: users.isAuthed})
+  ({users}) => ({isAuthed: users.isAuthed}),
+  (dispatch) => bindActionCreators(usersLikesActionCreators, dispatch)
 )(MainContainer)
