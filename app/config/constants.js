678981a90a7c55fb2be43fb0a703b73fe3e6d954
Tyler McGinnis <tylermcginnis33@gmail.com>
2016-04-09 13:40:31 -0600

    22-profile

--- app/config/constants.js
+++ app/config/constants.js
@@ -2,3 +2,6 @@ import Firebase from 'firebase'
 
 const firebaseUrl = 'https://duckr.firebaseio.com/'
 export const ref = new Firebase(firebaseUrl)
+
+export const usersDucksExpirationLength = 100000
+export const userExpirationLength = 100000
